43a7c0d7dde56277a3604364ca848a638938db51
Tobi Akere <148399418+Tobidevs@users.noreply.github.com>
2025-06-11 18:07:15 -0500

    Create user schema

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import { connectDB } from './db/connectDB.js'
+import { connectDB } from './db/connectDB.js';
+import cors from 'cors'
+import authRoutes from './routes/authRoutes.js'
 
 dotenv.config()
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
+app.use("/api/auth", authRoutes)
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     connectDB()
     console.log(`Server running on Port:${PORT}`)
